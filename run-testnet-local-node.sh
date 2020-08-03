e701ec25a12652ac6117a6235dcaa0abc9e978a7
Greg Zaitsev <greg.zaitsev@gmail.com>
2020-08-03 18:02:24 +0300

    Fix script to run local synced node of TestNet

--- run-testnet-local-node.sh
+++ run-testnet-local-node.sh
@@ -1,9 +1,7 @@
-BOOTNODES="--bootnodes /ip4/212.248.19.53/tcp/30334/p2p/12D3KooWRLrjXxByPkSCzcsRyf6652brnJT9s4AQHR3uujJ35mxz";
+BOOTNODES="--bootnodes /ip4/212.248.19.53/tcp/30334/p2p/12D3KooWHQDopuFB5krJGPQYn8Pg8i6toMeZFHGMs6sUPPmv4RhP";
 
 ./target/debug/nft \
-  --base-path /chain-data \
+  --base-path ./chain-data \
   --chain ./nftTestnetSpecRaw.json \
-  --ws-external \
-  --rpc-cors all \
   -lruntime \
   $BOOTNODES;
