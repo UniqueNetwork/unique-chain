275806de5a219956ef399062691a1f3f2bc76bb6
Greg Zaitsev <greg.zaitsev@gmail.com>
2020-08-31 16:00:46 +0300

    Add collection creation example

---
+++ examples/re-fungible/config.js
@@ -0,0 +1,19 @@
+// Local
+// const config = {
+//   wsEndpoint : 'ws://127.0.0.1:9944',
+//   collectionDataSize: 0,
+//   decimalPoints: 4,
+
+//   ownerSeed : '//Alice',
+// };
+
+// Production
+const config = {
+  wsEndpoint : 'wss://unique.usetech.com',
+  collectionDataSize: 0,
+  decimalPoints: 4,
+
+  ownerSeed : 'tunnel hair company air cage velvet egg crunch height fetch resource estate',
+};
+
+module.exports = config;
