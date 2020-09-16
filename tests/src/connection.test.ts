d02af7b15c14a3657699e61e5bb65afcc7b0b0a0
sotmorskiy <sotmorskiy@usetech.ru>
2020-09-16 11:31:38 +0700

    Disconnect detection test.

--- tests/src/connection.test.ts
+++ tests/src/connection.test.ts
@@ -1,5 +1,11 @@
-import {expect} from 'chai';
 import usingApi from "./substrate/substrate-api";
+import { WsProvider } from '@polkadot/api';
+import * as chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+
+chai.use(chaiAsPromised);
+
+const expect = chai.expect;
 
 describe('Connection', () => {
   it('Connection can be established', async () => {
@@ -7,5 +13,14 @@ describe('Connection', () => {
       const health = await api.rpc.system.health();
       expect(health).to.be.not.empty;
     });
+  });
+
+  it('Cannot connect to 0.0.0.0', () => {
+    const neverConnectProvider = new WsProvider('ws://0.0.0.0:9944');
+    expect((async () => {
+      await usingApi(async api => {
+        const health = await api.rpc.system.health();
+      }, { provider: neverConnectProvider });
+    })()).to.be.eventually.rejected;
   });
 });
--- tests/src/substrate/substrate-api.ts
+++ tests/src/substrate/substrate-api.ts
@@ -3,13 +3,17 @@ import config from "../config";
 import promisifySubstrate from "./promisify-substrate";
 import { ApiOptions } from "@polkadot/api/types";
 
-export default async function usingApi(action: (api: ApiPromise) => Promise<void>): Promise<void> {
+function defaultApiOptions(): ApiOptions {
   const wsProvider = new WsProvider(config.substrateUrl);
-  const settings: ApiOptions = { provider: wsProvider, types: JSON.parse(config.customTypes) };
+  return { provider: wsProvider, types: JSON.parse(config.customTypes) };
+}
+
+export default async function usingApi(action: (api: ApiPromise) => Promise<void>, settings: ApiOptions | undefined = undefined): Promise<void> {
+  settings = settings || defaultApiOptions();
   let api: ApiPromise | undefined = undefined;
 
   try {
-    api = await ApiPromise.create(settings);
+    api = new ApiPromise(settings);
     await promisifySubstrate(api, () => api && api.isReady)();
     await action(api);
   } finally {
