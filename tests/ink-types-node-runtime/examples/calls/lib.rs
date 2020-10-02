861b9092fa33e80ef05a5819690da1d31f59f920
sotmorskiy <sotmorskiy@usetech.ru>
2020-10-02 17:54:02 +0700

    NFTPAR-94: SIT: Balances can be transferred using smart contract.

---
+++ tests/ink-types-node-runtime/examples/calls/lib.rs
@@ -0,0 +1,48 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use ink_lang as ink;
+
+#[ink::contract(version = "0.1.0", env = NodeRuntimeTypes)]
+mod calls {
+    use ink_core::env;
+    use ink_prelude::*;
+    use ink_types_node_runtime::{calls as runtime_calls, NodeRuntimeTypes};
+
+    /// This simple dummy contract dispatches substrate runtime calls
+    #[ink(storage)]
+    struct Calls {}
+
+    impl Calls {
+        #[ink(constructor)]
+        fn new(&mut self) {}
+
+        /// Dispatches a `transfer` call to the Balances srml module
+        #[ink(message)]
+        fn balance_transfer(&self, dest: AccountId, value: Balance) {
+            // create the Balances::transfer Call
+            let transfer_call = runtime_calls::transfer_balance(dest, value);
+            
+            // dispatch the call to the runtime
+            let result = self.env().invoke_runtime(&transfer_call);
+
+            // report result to console
+            // NOTE: println should only be used on a development chain)
+            env::println(&format!("Balance transfer invoke_runtime result {:?}", result));
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+        use sp_keyring::AccountKeyring;
+
+        #[test]
+        fn dispatches_balances_call() {
+            let calls = Calls::new();
+            let alice = AccountId::from(AccountKeyring::Alice.to_account_id());
+            // assert_eq!(calls.env().dispatched_calls().into_iter().count(), 0);
+            calls.balance_transfer(alice, 10000);
+            // assert_eq!(calls.env().dispatched_calls().into_iter().count(), 1);
+        }
+    }
+}
--- tests/src/transfer.test.ts
+++ tests/src/transfer.test.ts
@@ -1,18 +1,10 @@
-import createSubstrateApi from "./substrate/substrate-api";
-import { Keyring, ApiPromise } from "@polkadot/api";
-import {AccountInfo} from "@polkadot/types/interfaces/system";
 import { expect } from "chai";
 import usingApi from "./substrate/substrate-api";
 import promisifySubstrate from "./substrate/promisify-substrate";
 import waitNewBlocks from "./substrate/wait-new-blocks";
 import { alicesPublicKey, bobsPublicKey } from "./accounts";
 import privateKey from "./substrate/privateKey";
-
-async function getBalance(api: ApiPromise, accounts: string[]): Promise<bigint[]> {
-  const balance = promisifySubstrate(api, (accounts: string[]) => api.query.system.account.multi(accounts));
-  const responce = await balance(accounts) as unknown as AccountInfo[];
-  return responce.map(r => r.data.free.toBigInt().valueOf());
-}
+import getBalance from "./substrate/get-balance";
 
 describe('Transfer', () => {
   it('Balance transfers', async () => {
@@ -27,7 +19,7 @@ describe('Transfer', () => {
 
       await waitNewBlocks(api);
   
-      const [alicesBalanceAfter, bobsBalanceAfter]  = await getBalance(api, [alicesPublicKey, bobsPublicKey]);
+      const [alicesBalanceAfter, bobsBalanceAfter] = await getBalance(api, [alicesPublicKey, bobsPublicKey]);
 
       expect(alicesBalanceAfter < alicesBalanceBefore).to.be.true;
       expect(bobsBalanceAfter > bobsBalanceBefore).to.be.true;
