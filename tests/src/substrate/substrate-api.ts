3c67ef33ab392b65ccc335697a799b232da0122e
Greg Zaitsev <greg.zaitsev@gmail.com>
2020-12-11 14:46:06 +0300

    Move runtime types to JSON file, update tests

--- tests/src/substrate/substrate-api.ts
+++ tests/src/substrate/substrate-api.ts
@@ -2,10 +2,13 @@ import { WsProvider, ApiPromise } from "@polkadot/api";
 import config from "../config";
 import promisifySubstrate from "./promisify-substrate";
 import { ApiOptions } from "@polkadot/api/types";
+import fs from "fs";
+
+const rtt = JSON.parse(fs.readFileSync('../runtime_types.json', 'utf8'));
 
 function defaultApiOptions(): ApiOptions {
   const wsProvider = new WsProvider(config.substrateUrl);
-  return { provider: wsProvider, types: JSON.parse(config.customTypes) };
+  return { provider: wsProvider, types: rtt };
 }
 
 export default async function usingApi(action: (api: ApiPromise) => Promise<void>, settings: ApiOptions | undefined = undefined): Promise<void> {
