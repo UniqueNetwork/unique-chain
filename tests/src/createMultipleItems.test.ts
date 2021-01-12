2c99b944ca7c6df6db8cfaeedba55f031dc7dd4c
kpozdnikin <kpozdnikin@usetech.ru>
2021-01-12 15:20:44 +0300

    createMultipleItems tests

--- tests/src/createMultipleItems.test.ts
+++ tests/src/createMultipleItems.test.ts
@@ -2,7 +2,7 @@
 // This file is subject to the terms and conditions defined in
 // file 'LICENSE', which is part of this source code package.
 //
-
+import { ApiPromise } from '@polkadot/api';
 import { assert } from 'chai';
 import { alicesPublicKey } from './accounts';
 import privateKey from './substrate/privateKey';
@@ -10,6 +10,52 @@ import usingApi from './substrate/substrate-api';
 import waitNewBlocks from './substrate/wait-new-blocks';
 
 const idCollection = 12;
+
+describe('Integration Test createMultipleItems(collection_id, owner, items_data):', () => {
+  it('Create  0x31, 0x32, 0x33 items in active NFT collection and verify tokens data in chain', async () => {
+    await usingApi(async (api: ApiPromise) => {
+
+    });
+  });
+});
+
+describe('Negative Integration Test createMultipleItems(collection_id, owner, items_data):', () => {
+  it('Create token with not existing type', async () => {
+    await usingApi(async (api: ApiPromise) => {
+
+    });
+  });
+
+  it('Create token in not existing collection', async () => {
+    await usingApi(async (api: ApiPromise) => {
+
+    });
+  });
+
+  it('Create token that has reached the maximum data limit', async () => {
+    await usingApi(async (api: ApiPromise) => {
+
+    });
+  });
+
+  it('Create tokens with different types', async () => {
+    await usingApi(async (api: ApiPromise) => {
+
+    });
+  });
+
+  it('Create tokens in not existing collection', async () => {
+    await usingApi(async (api: ApiPromise) => {
+
+    });
+  });
+
+  it('Create tokens with different data limits <> maximum data limit', async () => {
+    await usingApi(async (api: ApiPromise) => {
+
+    });
+  });
+});
 
 describe.skip('integration test: ext. createMultipleItems():', () => {
   it('Create two NFT tokens in active NFT collection', async () => {
