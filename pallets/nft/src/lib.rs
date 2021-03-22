ec67731bb0e14d82b64f8928a102779cc3401535
Yaroslav Bolyukin <iam@lach.pw>
2021-03-22 14:44:37 +0300

    feat: emit Transfer event on transfer_from

--- pallets/nft/src/lib.rs
+++ pallets/nft/src/lib.rs
@@ -1371,12 +1371,13 @@ decl_module! {
 
             match target_collection.mode
             {
-                CollectionMode::NFT => Self::transfer_nft(&target_collection, item_id, from, recipient)?,
+                CollectionMode::NFT => Self::transfer_nft(&target_collection, item_id, from.clone(), recipient.clone())?,
                 CollectionMode::Fungible(_)  => Self::transfer_fungible(&target_collection, value, &from, &recipient)?,
-                CollectionMode::ReFungible  => Self::transfer_refungible(&target_collection, item_id, value, from.clone(), recipient)?,
+                CollectionMode::ReFungible  => Self::transfer_refungible(&target_collection, item_id, value, from.clone(), recipient.clone())?,
                 _ => ()
             };
 
+            Self::deposit_event(RawEvent::Transfer(target_collection.id, item_id, from, recipient, value));
             Ok(())
         }
 
