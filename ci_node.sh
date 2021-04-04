ef7da6887d98c2527dc2f2f50f8ddfdf25a539b0
akaryaginUSETECH <akaryagin@usetech.ru>
2021-04-04 14:25:11 +0300

    updating ci

--- ci_node.sh
+++ ci_node.sh
@@ -3,8 +3,8 @@
 docker-compose -f docker-compose-tests.yml up -d --build --force-recreate --no-deps
 
 # validate if testing container finished
-while [ "$(docker ps | grep unit_test | wc -l)" -ge 1 ]; do echo "Unit test in process" && date && sleep 60; done && docker logs unit_test &> /home/polkadot/unit_test.log  & \
-while [ "$(docker ps | grep integration_test | wc -l)" -ge 1 ]; do echo "Integration test in process" && sleep 60; done && docker logs integration_test &> /home/polkadot/integration_test.log
+while [ "$(docker ps | grep unit_test | wc -l)" -ge 1 ]; do echo "Unit tests in process" && sleep 600; done && docker logs unit_test &> /home/polkadot/unit_test.log  & \
+while [ "$(docker ps | grep integration_test | wc -l)" -ge 1 ]; do echo "Integration tests in process" && sleep 600; done && docker logs integration_test &> /home/polkadot/integration_test.log
 docker-compose -f docker-compose-tests.yml down -v
 echo "Wrokflow finished"
 docker system prune --force
