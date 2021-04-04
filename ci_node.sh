d6eb853a8c1b3f8db186a718b9c465e8c1e2f1ae
akaryaginUSETECH <akaryagin@usetech.ru>
2021-04-04 19:07:12 +0300

    fix3

--- ci_node.sh
+++ ci_node.sh
@@ -8,5 +8,5 @@ docker-compose -f docker-compose-tests.yml up -d --build
 while [ "$(docker ps | grep unit_test | wc -l)" -ge 1 ]; do echo "Unit tests in process" && sleep 600; done && docker logs unit_test &> /home/polkadot/mochawesome-report/unit_test.log  & \
 while [ "$(docker ps | grep integration_test | wc -l)" -ge 1 ]; do echo "Integration tests in process" && sleep 600; done && docker logs integration_test &> /home/polkadot/mochawesome-report/integration_test.log
 docker-compose -f docker-compose-tests.yml down -v
-echo "Wrokflow finished"
+echo "Workflow finished"
 docker system prune --force
