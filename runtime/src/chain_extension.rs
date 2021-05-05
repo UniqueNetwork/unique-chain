397f5bd1733f6bdbd077262802008e0fa4aafc02
Yaroslav Bolyukin <iam@lach.pw>
2021-05-05 19:10:00 +0300

    refactor: remove unnecessary reencoding in CE
    Signed-off-by: Yaroslav Bolyukin <iam@lach.pw>

--- runtime/src/chain_extension.rs
+++ runtime/src/chain_extension.rs
@@ -44,6 +44,7 @@ pub struct NFTExtension;
 impl<C: Config> ChainExtension<C> for NFTExtension {
     fn call<E: Ext>(func_id: u32, env: Environment<E, InitState>) -> Result<RetVal, DispatchError>
     where
+        E: Ext<T = C>,
         <E::T as SysConfig>::AccountId: UncheckedFrom<<E::T as SysConfig>::Hash> + AsRef<[u8]>,
     {
         // The memory of the vm stores buf in scale-codec
@@ -52,25 +53,15 @@ impl<C: Config> ChainExtension<C> for NFTExtension {
                 let mut env = env.buf_in_buf_out();
                 let input: NFTExtTransfer<E> = env.read_as()?;
 
-                // Sender to AccountId32
-                let mut bytes_sender: [u8; 32] = [0; 32];
-                let addr_vec_sender: Vec<u8> = env.ext().caller().encode();
-                for i in 0..32 {
-                    bytes_sender[i] = addr_vec_sender[i];
-                }
-                let sender = AccountId32::from(bytes_sender);
+                let collection = pallet_nft::Module::<C>::get_collection(input.collection_id)?;
 
-                // Recipient to AccountId32
-                let mut bytes_rec: [u8; 32] = [0; 32];
-                let addr_vec_rec: Vec<u8> = input.recipient.encode();
-                for i in 0..32 {
-                    bytes_rec[i] = addr_vec_rec[i];
-                }
-                let recipient = AccountId32::from(bytes_rec);
-
-                let collection = pallet_nft::Module::<Runtime>::get_collection(input.collection_id)?;
-
-                match pallet_nft::Module::<Runtime>::transfer_internal(sender, recipient, &collection, input.token_id, input.amount) {
+                match pallet_nft::Module::<C>::transfer_internal(
+                    env.ext().caller().clone(),
+                    input.recipient,
+                    &collection,
+                    input.token_id,
+                    input.amount,
+                ) {
                     Ok(_) => Ok(RetVal::Converging(func_id)),
                     _ => Err(DispatchError::Other("Transfer error"))
                 }
@@ -80,31 +71,12 @@ impl<C: Config> ChainExtension<C> for NFTExtension {
                 let mut env = env.buf_in_buf_out();
                 let input: NFTExtCreateItem<E> = env.read_as()?;
 
-                // Contract address to AccountId32
-                let mut bytes_contract: [u8; 32] = [0; 32];
-                let addr_vec_contract: Vec<u8> = env.ext().address().encode();
-                for i in 0..32 {
-                    bytes_contract[i] = addr_vec_contract[i];
-                }
-                let sender = AccountId32::from(bytes_contract);
-
-                // Recipient to AccountId32
-                let mut bytes_rec: [u8; 32] = [0; 32];
-                let addr_vec_rec: Vec<u8> = input.owner.encode();
-                for i in 0..32 {
-                    bytes_rec[i] = addr_vec_rec[i];
-                }
-                let recipient = AccountId32::from(bytes_rec);
-
-                // Parse data
-                let cdata = input.data.encode();
-                let vdata: [u8; 0] = [0; 0];
-                let item_data = CreateItemData::NFT(CreateNftData{
-                    const_data: cdata,
-                    variable_data: vdata.to_vec()
-                });
-
-                match pallet_nft::Module::<Runtime>::create_item_internal(sender, input.collection_id, recipient, item_data) {
+                match pallet_nft::Module::<C>::create_item_internal(
+                    env.ext().address().clone(),
+                    input.collection_id,
+                    input.owner,
+                    input.data,
+                ) {
                     Ok(_) => Ok(RetVal::Converging(func_id)),
                     _ => Err(DispatchError::Other("CreateItem error"))
                 }
