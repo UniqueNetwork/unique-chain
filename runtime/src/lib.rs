3f998b38901501239ae97e64842c4ebb89275ab0
Greg Zaitsev <greg.zaitsev@gmail.com>
2021-07-14 15:00:38 +0300

    Fix block time

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -154,7 +154,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	transaction_version: 1,
 };
 
-pub const MILLISECS_PER_BLOCK: u64 = 1200;
+pub const MILLISECS_PER_BLOCK: u64 = 12000;
 
 pub const SLOT_DURATION: u64 = MILLISECS_PER_BLOCK;
 
