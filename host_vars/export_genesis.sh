9902b52b0ebd16867e9472e541b140e9ba38263e
Igor Kozyrev <igor@kozyrev.dev>
2021-09-02 15:08:49 +0000

    server setup

---
+++ host_vars/export_genesis.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+/opt/usetech/nft export-genesis-state --parachain-id 2101 > /opt/usetech/nft-parachain-2101-genesis
+/opt/usetech/nft export-genesis-wasm > /opt/usetech/nft-parachain-2101-wasm
