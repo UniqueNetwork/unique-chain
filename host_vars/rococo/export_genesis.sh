217dcd6a008e81d778d20aaf283e6fbbcaef0ebe
Igor Kozyrev <igor@kozyrev.dev>
2021-09-05 17:54:24 +0000

    add more settings

--- host_vars/rococo/export_genesis.sh
+++ host_vars/rococo/export_genesis.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-/opt/usetech/nft export-genesis-state --parachain-id 2101 > /opt/usetech/nft-parachain-2101-genesis
-/opt/usetech/nft export-genesis-wasm > /opt/usetech/nft-parachain-2101-wasm
+/opt/usetech/nft export-genesis-state --parachain-id 2000 > /opt/usetech/nft-parachain-2000-genesis
+/opt/usetech/nft export-genesis-wasm > /opt/usetech/nft-parachain-2000-wasm
