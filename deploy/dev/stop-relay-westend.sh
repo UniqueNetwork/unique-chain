371988ead6c5e0d198801768d0e5bf564ab3ecde
Igor Kozyrev <igor@kozyrev.dev>
2021-09-17 10:26:13 +0000

    dev setup westend-local relay

--- deploy/dev/stop-relay-westend.sh
+++ deploy/dev/stop-relay-westend.sh
@@ -1,24 +1,10 @@
 #!/bin/bash
+service relay-westend-ferdie stop
+service relay-westend-eve stop
 service relay-westend-dave stop
-sleep 5
-
 service relay-westend-charlie stop
-sleep 5
-
 service relay-westend-bob stop
-sleep 5
-
 service relay-westend-alice stop
 sleep 15
 
-rm -rfv /var/local/westend/dave/chains/westend_local_testnet/db
-rm -rfv /var/local/westend/dave/chains/westend_local_testnet/keystore
-
-rm -rfv /var/local/westend/charlie/chains/westend_local_testnet/db
-rm -rfv /var/local/westend/charlie/chains/westend_local_testnet/keystore
-
-rm -rfv /var/local/westend/bob/chains/westend_local_testnet/db
-rm -rfv /var/local/westend/bob/chains/westend_local_testnet/keystore
-
-rm -rfv /var/local/westend/alice/chains/westend_local_testnet/db
-rm -rfv /var/local/westend/alice/chains/westend_local_testnet/keystore
+rm -rfv /var/local/westend/
