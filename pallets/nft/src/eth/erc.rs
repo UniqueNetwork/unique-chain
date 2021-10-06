1d1711167fecc898c15f3f54a2b92951139cb50f
Yaroslav Bolyukin <iam@lach.pw>
2021-10-06 13:56:45 +0300

    fix: evm handling of burn_item
    Signed-off-by: Yaroslav Bolyukin <iam@lach.pw>

--- pallets/nft/src/eth/erc.rs
+++ pallets/nft/src/eth/erc.rs
@@ -194,7 +194,8 @@ impl<T: Config> CollectionHandle<T> {
 		let caller = T::CrossAccountId::from_eth(caller);
 		let token_id = token_id.try_into().map_err(|_| "amount overflow")?;
 
-		<Module<T>>::burn_item_internal(&caller, &self, token_id, &caller, 1).map_err(|_| "burn error")?;
+		<Module<T>>::burn_item_internal(&caller, &self, token_id, 1, true)
+			.map_err(|_| "burn error")?;
 		Ok(())
 	}
 }
