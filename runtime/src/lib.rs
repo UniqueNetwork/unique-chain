570dda64f497200e8bd2e2c511938072091b6f2e
Igor Kozyrev <igor@kozyrev.dev>
2021-10-06 14:01:34 +0000

    increment runtime spec_version

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -151,7 +151,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("nft"),
     impl_name: create_runtime_str!("nft"),
     authoring_version: 1,
-    spec_version: 3,
+    spec_version: 4,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 1,
