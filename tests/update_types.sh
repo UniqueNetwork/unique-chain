26aa849f1ff0e7437279cca5b5437cbda234ec13
Igor Kozyrev <igor@kozyrev.dev>
2021-12-02 22:23:51 +0000

    change repo path

--- tests/update_types.sh
+++ tests/update_types.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env sh
 
-if [[ ! -f unique-types-js ]]; then
+yarn polkadot-types
+
+cd ../..
+if [ ! -f unique-types-js ]; then
 	git clone git@github.com:UniqueNetwork/unique-types-js.git
 fi
 
-yarn polkadot-types
-
-rsync -ar --delete src/interfaces/ unique-types-js/src/interfaces
+rsync -ar --delete unique-chain/tests/src/interfaces/ unique-types-js/src/interfaces
 for file in unique-types-js/src/interfaces/augment-*; do
 	sed -i '1s;^;//@ts-nocheck\n;' $file
 done
 
-pushd unique-types-js
+cd unique-types-js
 git add src/interfaces/
 git commit -m "chore: regenerate types"
-popd
+cd ../unique-chain/tests
