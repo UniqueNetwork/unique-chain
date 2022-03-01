2a0daba548429fe4498be9a33215d1ce252a45f6
Igor Kozyrev <igor@kozyrev.dev>
2022-03-01 08:22:19 +0000

    up runtime version

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -161,7 +161,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("opal"),
 	impl_name: create_runtime_str!("opal"),
 	authoring_version: 1,
-	spec_version: 916010,
+	spec_version: 917000,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
