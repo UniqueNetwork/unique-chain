a88ba9715a5499ec7d6bcb642c1c39ba38bae9a8
Greg Zaitsev <greg.zaitsev@gmail.com>
2022-03-03 16:51:12 +0300

    Update license

--- tests/src/interfaces/definitions.ts
+++ tests/src/interfaces/definitions.ts
@@ -1,1 +1,17 @@
+// Copyright 2019-2022 Unique Network (Gibraltar) Ltd.
+// This file is part of Unique Network.
+
+// Unique Network is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Unique Network is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Unique Network. If not, see <http://www.gnu.org/licenses/>.
+
 export {default as unique} from './unique/definitions';
