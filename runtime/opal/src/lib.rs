6d40a57a360c27d1d6752801574a3c67b8461d24
Igor Kozyrev <igor@kozyrev.dev>
2022-03-30 11:06:33 +0000

    fix manual merge

--- runtime/opal/src/lib.rs
+++ runtime/opal/src/lib.rs
@@ -216,15 +216,11 @@ parameter_types! {
 		.avg_block_initialization(AVERAGE_ON_INITIALIZE_RATIO)
 		.build_or_panic();
 	pub const Version: RuntimeVersion = VERSION;
-	/*
-	255 - Quartz
-	42 - Opal
-	*/
-	pub const SS58Prefix: u8 = 255;
+	pub const SS58Prefix: u8 = 42;
 }
 
 parameter_types! {
-	pub const ChainId: u64 = 8881;
+	pub const ChainId: u64 = 8882;
 }
 
 pub struct FixedFee;
