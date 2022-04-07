c31b5ba3b03c4ca7aa0284a3ce5bc67c2e8501b3
Yaroslav Bolyukin <iam@lach.pw>
2022-04-07 13:14:50 +0300

    feat: implement collection nesting rules field
    Signed-off-by: Yaroslav Bolyukin <iam@lach.pw>

--- pallets/common/src/lib.rs
+++ pallets/common/src/lib.rs
@@ -192,7 +192,10 @@ pub mod pallet {
 		type CrossTokenAddressMapping: TokenAddressMapping<Self::CrossAccountId>;
 	}
 
+	const STORAGE_VERSION: StorageVersion = StorageVersion::new(1);
+
 	#[pallet::pallet]
+	#[pallet::storage_version(STORAGE_VERSION)]
 	#[pallet::generate_store(pub(super) trait Store)]
 	pub struct Pallet<T>(_);
 
@@ -393,6 +396,22 @@ pub mod pallet {
 	#[pallet::storage]
 	pub type DummyStorageValue<T> =
 		StorageValue<Value = (CollectionStats, CollectionId, TokenId), QueryKind = OptionQuery>;
+
+	#[pallet::hooks]
+	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {
+		fn on_runtime_upgrade() -> Weight {
+			let mut weight = 0;
+
+			if StorageVersion::get::<Pallet<T>>() < StorageVersion::new(1) {
+				use up_data_structs::{CollectionVersion1, CollectionVersion2};
+				<CollectionById<T>>::translate_values::<CollectionVersion1<T::AccountId>, _>(|v| {
+					Some(CollectionVersion2::from(v))
+				});
+			}
+
+			weight
+		}
+	}
 }
 
 impl<T: Config> Pallet<T> {
