dee9f7d69f478c362c1a637cabe86426fc4de314
Yaroslav Bolyukin <iam@lach.pw>
2022-04-07 13:15:42 +0300

    test: basic bundle transfer
    Signed-off-by: Yaroslav Bolyukin <iam@lach.pw>

---
+++ tests/src/nesting/nest.test.ts
@@ -0,0 +1,31 @@
+import {expect} from 'chai';
+import {tokenIdToAddress} from '../eth/util/helpers';
+import privateKey from '../substrate/privateKey';
+import usingApi from '../substrate/substrate-api';
+import {createCollectionExpectSuccess, createItemExpectSuccess, getTokenOwner, setCollectionLimitsExpectSuccess, transferExpectSuccess, transferFromExpectSuccess} from '../util/helpers';
+
+describe('nesting', () => {
+  it('allows to nest/unnest token', async () => {
+    await usingApi(async api => {
+      const alice = privateKey('//Alice');
+      const bob = privateKey('//Bob');
+
+      const collection = await createCollectionExpectSuccess({mode: {type: 'NFT'}});
+      await setCollectionLimitsExpectSuccess(alice, collection, {nestingRule:{OwnerRestricted:[collection]}});
+      const targetToken = await createItemExpectSuccess(alice, collection, 'NFT');
+
+      const nestedToken = await createItemExpectSuccess(alice, collection, 'NFT');
+
+      // Nest
+      await transferExpectSuccess(collection, nestedToken, alice, {Ethereum: tokenIdToAddress(collection, targetToken)});
+
+      // Move bundle to different user
+      await transferExpectSuccess(collection, targetToken, alice, {Substrate: bob.address});
+
+      // Unnest
+      await transferFromExpectSuccess(collection, nestedToken, bob, {Ethereum: tokenIdToAddress(collection, targetToken)}, {Substrate: bob.address});
+
+      expect(await getTokenOwner(api, collection, nestedToken)).to.be.deep.equal({Substrate: bob.address});
+    });
+  });
+});
