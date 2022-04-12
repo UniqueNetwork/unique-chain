f082ca53dc4ec0af8aa60d844d52959c041c4105
Yaroslav Bolyukin <iam@lach.pw>
2022-04-12 12:56:31 +0300

    test: apps launch scripts
    Signed-off-by: Yaroslav Bolyukin <iam@lach.pw>

--- tests/update_types.sh
+++ tests/update_types.sh
@@ -2,17 +2,16 @@
 
 yarn polkadot-types
 
-cd ../..
-if [ ! -f unique-types-js ]; then
+if [ ! -d unique-types-js ]; then
 	git clone git@github.com:UniqueNetwork/unique-types-js.git
 fi
 
-rsync -ar --delete unique-chain/tests/src/interfaces/ unique-types-js/src/interfaces
-for file in unique-types-js/src/interfaces/augment-*; do
+rsync -ar --exclude .gitignore src/interfaces/ unique-types-js
+for file in unique-types-js/augment-*; do
 	sed -i '1s;^;//@ts-nocheck\n;' $file
 done
 
-cd unique-types-js
-git add src/interfaces/
+pushd unique-types-js
+git add .
 git commit -m "chore: regenerate types"
-cd ../unique-chain/tests
+popd
