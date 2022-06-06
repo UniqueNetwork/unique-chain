fea2ecf542d1e14cddd818d1c41743f9d7e3b96c
Trubnikov Sergey <bugrazoid@yandex.ru>
2022-06-06 06:15:08 +0000

    CORE-386 Fix lang

--- pallets/nonfungible/src/lib.rs
+++ pallets/nonfungible/src/lib.rs
@@ -497,7 +497,7 @@ impl<T: Config> Pallet<T> {
 				token_owner,
 				..
 			} => {
-				//TODO: исследовать угрозы при публичном минтинге.
+				//TODO: investigate threats during public minting.
 				if is_token_create && (collection_admin || token_owner) {
 					return Ok(());
 				}
