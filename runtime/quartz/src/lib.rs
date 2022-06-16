5ae8aa5142b8cff804f14117c18e045919469764
Igor Kozyrev <igor@kozyrev.dev>
2022-06-16 05:36:46 +0000

    runtime version up

--- runtime/quartz/src/lib.rs
+++ runtime/quartz/src/lib.rs
@@ -192,7 +192,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!(RUNTIME_NAME),
 	impl_name: create_runtime_str!(RUNTIME_NAME),
 	authoring_version: 1,
-	spec_version: 922000,
+	spec_version: 922080,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
