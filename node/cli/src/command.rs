450113e32faf60bb8b30825be653d1bcfbd05619
Trubnikov Sergey <bugrazoid@yandex.ru>
2022-06-17 09:03:40 +0000

    Fix after rebase

--- node/cli/src/command.rs
+++ node/cli/src/command.rs
@@ -495,7 +495,7 @@ pub fn run() -> Result<()> {
 				let para_id = ParaId::from(para_id);
 
 				let parachain_account =
-					AccountIdConversion::<polkadot_primitives::v2::AccountId>::into_account(
+					AccountIdConversion::<polkadot_primitives::v2::AccountId>::into_account_truncating(
 						&para_id,
 					);
 
