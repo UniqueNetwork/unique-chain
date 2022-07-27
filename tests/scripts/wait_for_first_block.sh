b5d49dbfae33d068efcdd9d058c1d4586c9b0aef
Alexander Aksenov <skydeman@gmail.com>
2022-07-27 16:57:36 +0300

    Added script for checking state of app in docker container

--- tests/scripts/wait_for_first_block.sh
+++ tests/scripts/wait_for_first_block.sh
@@ -1,26 +1,34 @@
 #!/usr/bin/env bash
 
-function do_rpc {
-	curl -s --header "Content-Type: application/json" -XPOST --data "{\"id\":1,\"jsonrpc\":\"2.0\",\"method\":\"$1\",\"params\":[$2]}" $RPC_URL
-}
+CONT_ID="node-o"
+TEMPLATE_STR='Imported #1'
+
 
 function is_started {
-    block_hash_rpc=$(do_rpc chain_getFinalizedHead)
-    echo Rpc response = $block_hash_rpc
-	block_hash=$(echo $block_hash_rpc | jq -r .result)
-	echo Head = $block_hash
-	block_id_hex=$(do_rpc chain_getHeader "\"$block_hash\"" | jq -r .result.number)
-	block_id=$((${block_id_hex}))
-	echo Id = $block_id
-	if (( $block_id > 1 )); then
-		return 0
-	fi
-	return 1
-}
+  OUTPUT=$(docker logs $CONT_ID 2>&1 | grep -i "$TEMPLATE_STR")
+ 
+  if [[ -z ${OUTPUT} ]];
+  then
+    echo "variable OUTPUT is not set"
+    return 1
+  else
+    echo "variable named OUTPUT is already set"
+    echo "Container ID: $CONT_ID"
+    echo "Template for lookup: $TEMPLATE_STR"
+    return 0
+  fi
+    echo "not match"
+    return 1
+  
+}                                                                                                                                   
 
-while ! is_started; do
-	echo "$RPC_URL"
-	echo "Waiting for first block..."
-	sleep 12
+while ! is_started;
+do
+  echo "Waiting for first block..."
+  sleep 12
 done
+
+                                                                                                                                        
 echo "Chain is running!"
+exit 0
+
