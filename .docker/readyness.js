8fcfaf37f8d357a683250638b29410592988126a
Alexander Aksenov <skydeman@gmail.com>
2022-08-04 17:50:30 +0300

    Readyness script update.

--- .docker/readyness.js
+++ .docker/readyness.js
@@ -3,7 +3,7 @@ const { ApiPromise, WsProvider } = require('@polkadot/api');
 const main = async () => {
   const wsEndpoint = 'ws://127.0.0.1:9944'
   const api = new ApiPromise({provider: new WsProvider(wsEndpoint)});
-  await api.isReady;
+  await api.isReadyOrError;
 
   const head = (await api.rpc.chain.getHeader()).number.toNumber();
   if(head < 1) throw Error('No block #1');
