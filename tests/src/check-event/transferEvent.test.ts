508677d90089c28bf3957806240f6885b8f73f5a
rkv <dontgofun@gmail.com>
2022-09-26 15:20:50 +0300

    transferEvent migrated

--- tests/src/check-event/transferEvent.test.ts
+++ tests/src/check-event/transferEvent.test.ts
@@ -15,15 +15,10 @@
 // along with Unique Network. If not, see <http://www.gnu.org/licenses/>.
 
 // https://unique-network.readthedocs.io/en/latest/jsapi.html#setchainlimits
-import {ApiPromise} from '@polkadot/api';
 import {IKeyringPair} from '@polkadot/types/types';
-import chai from 'chai';
-import chaiAsPromised from 'chai-as-promised';
-import usingApi, {submitTransactionAsync} from '../substrate/substrate-api';
-import {createCollectionExpectSuccess, createItemExpectSuccess, uniqueEventMessage, normalizeAccountId} from '../util/helpers';
-
-chai.use(chaiAsPromised);
-const expect = chai.expect;
+import {executeTransaction} from '../substrate/substrate-api';
+import {uniqueEventMessage, normalizeAccountId} from '../util/helpers';
+import {usingPlaygrounds, expect, itSub} from '../util/playgrounds';
 
 describe('Transfer event ', () => {
   let alice: IKeyringPair;
@@ -31,22 +26,22 @@ describe('Transfer event ', () => {
   const checkSection = 'Transfer';
   const checkTreasury = 'Deposit';
   const checkSystem = 'ExtrinsicSuccess';
+
   before(async () => {
-    await usingApi(async (api, privateKeyWrapper) => {
-      alice = privateKeyWrapper('//Alice');
-      bob = privateKeyWrapper('//Bob');
+    await usingPlaygrounds(async (helper, privateKey) => {
+      const donor = privateKey('//Alice');
+      [alice, bob] = await helper.arrange.createAccounts([10n, 10n], donor);
     });
   });
-  it('Check event from transfer(): ', async () => {
-    await usingApi(async (api: ApiPromise) => {
-      const collectionID = await createCollectionExpectSuccess();
-      const itemID = await createItemExpectSuccess(alice, collectionID, 'NFT');
-      const transfer = api.tx.unique.transfer(normalizeAccountId(bob.address), collectionID, itemID, 1);
-      const events = await submitTransactionAsync(alice, transfer);
-      const msg = JSON.stringify(uniqueEventMessage(events));
-      expect(msg).to.be.contain(checkSection);
-      expect(msg).to.be.contain(checkTreasury);
-      expect(msg).to.be.contain(checkSystem);
-    });
+
+  itSub('Check event from transfer(): ', async ({helper}) => {
+    const collection = await helper.nft.mintCollection(alice, {name: 'test', description: 'test', tokenPrefix: 'test'});
+    const {tokenId} = await collection.mintToken(alice, {Substrate: alice.address});
+    const transferTx = helper.api!.tx.unique.transfer(normalizeAccountId(bob.address), collection.collectionId, tokenId, 1);
+    const events = await executeTransaction(helper.api!, alice, transferTx);
+    const msg = JSON.stringify(uniqueEventMessage(events));
+    expect(msg).to.be.contain(checkSection);
+    expect(msg).to.be.contain(checkTreasury);
+    expect(msg).to.be.contain(checkSystem);
   });
 });
