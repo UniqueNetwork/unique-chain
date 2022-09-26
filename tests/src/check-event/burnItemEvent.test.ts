99f3d75dc724b7c79091d914224f63d74bad49fe
rkv <dontgofun@gmail.com>
2022-09-26 14:52:57 +0300

    burnItemEvent migrated

--- tests/src/check-event/burnItemEvent.test.ts
+++ tests/src/check-event/burnItemEvent.test.ts
@@ -15,15 +15,11 @@
 // along with Unique Network. If not, see <http://www.gnu.org/licenses/>.
 
 // https://unique-network.readthedocs.io/en/latest/jsapi.html#setchainlimits
-import {ApiPromise} from '@polkadot/api';
 import {IKeyringPair} from '@polkadot/types/types';
-import chai from 'chai';
-import chaiAsPromised from 'chai-as-promised';
-import usingApi, {submitTransactionAsync} from '../substrate/substrate-api';
-import {createCollectionExpectSuccess, createItemExpectSuccess, uniqueEventMessage} from '../util/helpers';
+import {executeTransaction} from '../substrate/substrate-api';
+import {uniqueEventMessage} from '../util/helpers';
+import {usingPlaygrounds, expect, itSub} from '../util/playgrounds';
 
-chai.use(chaiAsPromised);
-const expect = chai.expect;
 
 describe('Burn Item event ', () => {
   let alice: IKeyringPair;
@@ -31,20 +27,20 @@ describe('Burn Item event ', () => {
   const checkTreasury = 'Deposit';
   const checkSystem = 'ExtrinsicSuccess';
   before(async () => {
-    await usingApi(async (api, privateKeyWrapper) => {
-      alice = privateKeyWrapper('//Alice');
+    await usingPlaygrounds(async (helper, privateKey) => {
+      const donor = privateKey('//Alice');
+      [alice] = await helper.arrange.createAccounts([10n], donor);
     });
   });
-  it('Check event from burnItem(): ', async () => {
-    await usingApi(async (api: ApiPromise) => {
-      const collectionID = await createCollectionExpectSuccess();
-      const itemID = await createItemExpectSuccess(alice, collectionID, 'NFT');
-      const burnItem = api.tx.unique.burnItem(collectionID, itemID, 1);
-      const events = await submitTransactionAsync(alice, burnItem);
-      const msg = JSON.stringify(uniqueEventMessage(events));
-      expect(msg).to.be.contain(checkSection);
-      expect(msg).to.be.contain(checkTreasury);
-      expect(msg).to.be.contain(checkSystem);
-    });
+  itSub('Check event from burnItem(): ', async ({helper}) => {
+    const collection = await helper.nft.mintCollection(alice, {name: 'test', description: 'test', tokenPrefix: 'test'});
+    const token = await collection.mintToken(alice, {Substrate: alice.address});
+
+    const tx = helper.api!.tx.unique.burnItem(collection.collectionId, token.tokenId, 1);
+    const events = await executeTransaction(helper.api!, alice, tx);
+    const msg = JSON.stringify(uniqueEventMessage(events));
+    expect(msg).to.be.contain(checkSection);
+    expect(msg).to.be.contain(checkTreasury);
+    expect(msg).to.be.contain(checkSystem);
   });
 });
