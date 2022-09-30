d59662d61f76c09a23689bd4411f955e4c312e01
Daniel Shiposha <dshiposha@usetech.com>
2022-09-30 09:17:56 +0000

    fix: remove unused imports

--- tests/src/getPropertiesRpc.test.ts
+++ tests/src/getPropertiesRpc.test.ts
@@ -15,8 +15,8 @@
 // along with Unique Network. If not, see <http://www.gnu.org/licenses/>.
 
 import {IKeyringPair} from '@polkadot/types/types';
-import {itSub, Pallets, requirePalletsOrSkip, usingPlaygrounds, expect} from './util/playgrounds';
-import {UniqueHelper, UniqueBaseCollection, UniqueNFTCollection, UniqueNFToken} from './util/playgrounds/unique';
+import {itSub, usingPlaygrounds, expect} from './util/playgrounds';
+import {UniqueHelper, UniqueNFTCollection} from './util/playgrounds/unique';
 
 const collectionProps = [
   {key: 'col-0', value: 'col-0-value'},
