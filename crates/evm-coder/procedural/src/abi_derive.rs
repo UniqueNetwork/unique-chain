0e1058776aefd3d51282ada973ca560b9fa264cb
Trubnikov Sergey <bugrazoid@yandex.ru>
2022-11-10 14:24:53 +0000

    refactor: change panic to syn::Error

--- crates/evm-coder/procedural/src/abi_derive.rs
+++ crates/evm-coder/procedural/src/abi_derive.rs
@@ -1,15 +1,15 @@
 use quote::quote;
 
-pub(crate) fn impl_abi_macro(ast: &syn::DeriveInput) -> proc_macro2::TokenStream {
+pub(crate) fn impl_abi_macro(ast: &syn::DeriveInput) -> syn::Result<proc_macro2::TokenStream> {
 	// dbg!(ast);
 	let name = &ast.ident;
 	let can_be_plcaed_in_vec = impl_can_be_placed_in_vec(name);
-	let abi_type = impl_abi_type(ast);
+	let abi_type = impl_abi_type(ast)?;
 	// println!("{}", abi_type);
-	quote! {
+	Ok(quote! {
 		#can_be_plcaed_in_vec
 		#abi_type
-	}
+	})
 }
 
 fn impl_can_be_placed_in_vec(ident: &syn::Ident) -> proc_macro2::TokenStream {
@@ -22,23 +22,23 @@ fn map_field_to_type<'a>(field: &'a syn::Field) -> &'a syn::Type {
 	&field.ty
 }
 
-fn impl_abi_type(ast: &syn::DeriveInput) -> proc_macro2::TokenStream {
+fn impl_abi_type(ast: &syn::DeriveInput) -> syn::Result<proc_macro2::TokenStream> {
 	let name = &ast.ident;
 	let (fields, params_count) = match &ast.data {
 		syn::Data::Struct(ds) => match ds.fields {
-			syn::Fields::Named(ref fields) => (
+			syn::Fields::Named(ref fields) => Ok((
 				fields.named.iter().map(map_field_to_type),
 				fields.named.len(),
-			),
-			syn::Fields::Unnamed(ref fields) => (
+			)),
+			syn::Fields::Unnamed(ref fields) => Ok((
 				fields.unnamed.iter().map(map_field_to_type),
 				fields.unnamed.len(),
-			),
-			syn::Fields::Unit => unimplemented!("Unit structs not supported"),
+			)),
+			syn::Fields::Unit => Err(syn::Error::new(name.span(), "Unit structs not supported")),
 		},
-		syn::Data::Enum(_) => unimplemented!("Enums not supported"),
-		syn::Data::Union(_) => unimplemented!("Unions not supported"),
-	};
+		syn::Data::Enum(_) => Err(syn::Error::new(name.span(), "Enums not supported")),
+		syn::Data::Union(_) => Err(syn::Error::new(name.span(), "Unions not supported")),
+	}?;
 
 	let mut params_signature = {
 		let fields = fields.clone();
@@ -46,13 +46,16 @@ fn impl_abi_type(ast: &syn::DeriveInput) -> proc_macro2::TokenStream {
 			#(nameof(<#fields as ::evm_coder::abi::AbiType>::SIGNATURE) fixed(","))*
 		)
 	};
-	if params_count > 0 {
-		params_signature.extend(quote!(shift_left(1)))
+
+	if params_count == 0 {
+		return Err(syn::Error::new(name.span(), "Empty structs not supported"));
 	};
+
+	params_signature.extend(quote!(shift_left(1)));
 
 	let fields_for_dynamic = fields.clone();
 
-	quote! {
+	Ok(quote! {
 		impl ::evm_coder::abi::AbiType for #name {
 			const SIGNATURE: ::evm_coder::custom_signature::SignatureUnit = ::evm_coder::make_signature!(
 				new fixed("(")
@@ -69,5 +72,5 @@ fn impl_abi_type(ast: &syn::DeriveInput) -> proc_macro2::TokenStream {
 				0 #(+ <#fields as ::evm_coder::abi::AbiType>::size())*
 			}
 		}
-	}
+	})
 }
