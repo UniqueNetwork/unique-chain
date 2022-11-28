edb12e69f3be8f601b67463d8540eef2e5b972e5
Konstantin Astakhov <kastakhov@usetech.com>
2022-11-29 00:45:05 +0700

    fix actions version

--- tests/scripts/wait_for_first_block.sh
+++ tests/scripts/wait_for_first_block.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+echo "Start waiting for first block"
 DIR=$(dirname "$0")
 
 . $DIR/functions.sh
@@ -7,7 +8,7 @@ DIR=$(dirname "$0")
 function is_started {
 	block_id_hex=$(do_rpc chain_getHeader | jq -r .result.number)
 	block_id=$((${block_id_hex}))
-	echo Id = $block_id
+	# echo Id = $block_id
 	if (( $block_id > 1 )); then
 		return 0
 	fi
@@ -15,7 +16,7 @@ function is_started {
 }
 
 while ! is_started; do
-	echo "Waiting for first block..."
+	# echo "Waiting for first block..."
 	sleep 12
 done
 echo "Chain is running!"
