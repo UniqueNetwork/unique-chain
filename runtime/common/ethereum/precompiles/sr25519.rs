a126dc7b44a8c24415db185cc760649146b9e3d7
Grigoriy Simonov <gsimonov@usetech.com>
2022-12-16 09:54:40 +0000

    fix: add PureStake license header

--- runtime/common/ethereum/precompiles/sr25519.rs
+++ runtime/common/ethereum/precompiles/sr25519.rs
@@ -1,3 +1,19 @@
+// Copyright 2019-2022 PureStake Inc.
+// Copyright 2022      Stake Technologies
+
+// Astar Network is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Astar Network is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Astar Network. If not, see <http://www.gnu.org/licenses/>.
+
 use fp_evm::{Context, ExitSucceed, PrecompileHandle, PrecompileOutput};
 use pallet_evm::Precompile;
 use sp_core::{crypto::UncheckedFrom, sr25519, H256};
