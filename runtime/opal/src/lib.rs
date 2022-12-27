0895f5c26bec1204d8a8701758ab2cdc5345f39b
Unique <github-actions@usetech.com>
2022-12-27 16:07:18 +0700

    fix runtime version for chains

--- runtime/opal/src/lib.rs
+++ runtime/opal/src/lib.rs
@@ -59,7 +59,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!(RUNTIME_NAME),
 	impl_name: create_runtime_str!(RUNTIME_NAME),
 	authoring_version: 1,
-	spec_version: 336040,
+	spec_version: 936040,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 3,
