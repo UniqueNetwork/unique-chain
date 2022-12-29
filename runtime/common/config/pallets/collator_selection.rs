f2a853a15fb657495101ecb96a5c09c6bc08aa5d
Fahrrader <fhakimov@usetech.com>
2022-12-29 10:11:14 +0000

    fix(collator-selection): proper weights

--- runtime/common/config/pallets/collator_selection.rs
+++ runtime/common/config/pallets/collator_selection.rs
@@ -94,5 +94,5 @@ impl pallet_collator_selection::Config for Runtime {
 	type ValidatorId = <Self as frame_system::Config>::AccountId;
 	type ValidatorIdOf = pallet_collator_selection::IdentityCollator;
 	type ValidatorRegistration = Session;
-	type WeightInfo = ();
+	type WeightInfo = pallet_collator_selection::weights::SubstrateWeight<Runtime>;
 }
