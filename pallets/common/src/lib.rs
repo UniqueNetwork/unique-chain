7a78f3ccc3ff7d534038347a4c57e578c8744784
Daniel Shiposha <dshiposha@usetech.com>
2023-01-12 13:55:21 +0000

    fix: use set instead of mutate

--- pallets/common/src/lib.rs
+++ pallets/common/src/lib.rs
@@ -1236,9 +1236,7 @@ impl<T: Config> Pallet<T> {
 			}
 		}
 
-		<CollectionProperties<T>>::mutate(collection.id, |properties| {
-			*properties = stored_properties;
-		});
+		<CollectionProperties<T>>::set(collection.id, stored_properties);
 
 		Ok(())
 	}
