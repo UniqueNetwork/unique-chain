e3dcd7b65003e8021540429bd654e1bffdc4cfc5
Daniel Shiposha <dshiposha@usetech.com>
2023-03-01 17:35:27 +0000

    fix: expose pallet xcm storage

--- runtime/common/construct_runtime.rs
+++ runtime/common/construct_runtime.rs
@@ -61,7 +61,7 @@ macro_rules! construct_runtime {
 
                 // XCM helpers.
                 XcmpQueue: cumulus_pallet_xcmp_queue::{Pallet, Call, Storage, Event<T>} = 50,
-                PolkadotXcm: pallet_xcm::{Pallet, Call, Event<T>, Origin} = 51,
+                PolkadotXcm: pallet_xcm::{Pallet, Call, Storage, Event<T>, Origin} = 51,
                 CumulusXcm: cumulus_pallet_xcm::{Pallet, Call, Event<T>, Origin} = 52,
                 DmpQueue: cumulus_pallet_dmp_queue::{Pallet, Call, Storage, Event<T>} = 53,
 
