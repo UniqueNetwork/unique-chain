d59d6287c7af3e8f3b37860ee7963cbb0afd64f9
Daniel Shiposha <dshiposha@usetech.com>
2023-03-06 08:58:50 +0000

    fix: all tech comm can suspend/resume xcm

--- runtime/common/config/xcm/mod.rs
+++ runtime/common/config/xcm/mod.rs
@@ -18,7 +18,6 @@ use frame_support::{
 	traits::{Everything, Get},
 	parameter_types,
 };
-use frame_system::EnsureRoot;
 use pallet_xcm::XcmPassthrough;
 use polkadot_parachain::primitives::Sibling;
 use xcm::latest::{prelude::*, Weight, MultiLocation};
@@ -47,6 +46,9 @@ pub use foreignassets as xcm_assets;
 
 #[cfg(not(feature = "foreign-assets"))]
 pub use nativeassets as xcm_assets;
+
+#[cfg(feature = "governance")]
+use crate::runtime_common::config::pallets::governance;
 
 use xcm_assets::{AssetTransactors, IsReserve, Trader};
 
@@ -257,7 +259,13 @@ impl cumulus_pallet_xcmp_queue::Config for Runtime {
 	type ChannelInfo = ParachainSystem;
 	type VersionWrapper = ();
 	type ExecuteOverweightOrigin = frame_system::EnsureRoot<AccountId>;
-	type ControllerOrigin = EnsureRoot<AccountId>;
+
+	#[cfg(feature = "governance")]
+	type ControllerOrigin = governance::RootOrAllTechnicalCommittee;
+
+	#[cfg(not(feature = "governance"))]
+	type ControllerOrigin = frame_system::EnsureRoot<AccountId>;
+
 	type ControllerOriginConverter = XcmOriginToTransactDispatchOrigin;
 }
 
