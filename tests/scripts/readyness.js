6e54658cad7de1daf3b3bb5e9377bc1765f67152
Yaroslav Bolyukin <iam@lach.pw>
2023-03-29 14:45:24 +0200

    test: migrate readyness script to es modules

--- tests/scripts/readyness.js
+++ tests/scripts/readyness.js
@@ -1,4 +1,4 @@
-const { ApiPromise, WsProvider } = require('@polkadot/api');
+import { ApiPromise, WsProvider } from '@polkadot/api';
 
 const connect = async () => {
   const wsEndpoint = 'ws://127.0.0.1:9944';
