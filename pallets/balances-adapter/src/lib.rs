7c75ab3aafa00c6c1e90c3282953ab18808f66ac
Trubnikov Sergey <bugrazoid@yandex.ru>
2023-04-19 13:32:02 +0000

    feat: add erc20 interface

--- pallets/balances-adapter/src/lib.rs
+++ pallets/balances-adapter/src/lib.rs
@@ -4,10 +4,12 @@
 
 pub use pallet::*;
 
+pub mod erc;
+
 #[frame_support::pallet]
 pub mod pallet {
 	#[pallet::config]
-	pub trait Config: frame_system::Config {}
+	pub trait Config: frame_system::Config + pallet_evm_coder_substrate::Config {}
 	#[pallet::pallet]
 	pub struct Pallet<T>(_);
 
