cc387ee572aae3ee99602e2464c304a8a67ebd2d
Trubnikov Sergey <bugrazoid@yandex.ru>
2023-04-28 12:43:03 +0000

    remove warnings

--- pallets/balances-adapter/src/lib.rs
+++ pallets/balances-adapter/src/lib.rs
@@ -1,11 +1,9 @@
 // #![doc = include_str!("../README.md")]
 #![cfg_attr(not(feature = "std"), no_std)]
-#![warn(missing_docs)]
 
 extern crate alloc;
 use frame_support::sp_runtime::DispatchResult;
 pub use pallet::*;
-use pallet_common::CollectionHandle;
 use pallet_evm_coder_substrate::{WithRecorder, SubstrateRecorder};
 
 pub mod common;
@@ -13,12 +11,15 @@ pub mod erc;
 
 pub(crate) type SelfWeightOf<T> = <T as Config>::WeightInfo;
 
+/// Handle for native fungible collection
 pub struct NativeFungibleHandle<T: Config>(SubstrateRecorder<T>);
 impl<T: Config> NativeFungibleHandle<T> {
+	/// Creates a handle
 	pub fn new() -> NativeFungibleHandle<T> {
 		Self(SubstrateRecorder::new(u64::MAX))
 	}
 
+	/// Check if the collection is internal
 	pub fn check_is_internal(&self) -> DispatchResult {
 		Ok(())
 	}
@@ -35,7 +36,7 @@ impl<T: Config> WithRecorder<T> for NativeFungibleHandle<T> {
 #[frame_support::pallet]
 pub mod pallet {
 	use alloc::string::String;
-	use frame_support::{traits::Get, sp_runtime::DispatchResult};
+	use frame_support::{traits::Get};
 	use pallet_balances::WeightInfo;
 	use sp_core::U256;
 
@@ -43,25 +44,24 @@ pub mod pallet {
 	pub trait Config:
 		frame_system::Config + pallet_evm_coder_substrate::Config + pallet_common::Config
 	{
+		/// Currency from `pallet_balances`
 		type Currency: frame_support::traits::Currency<
 			Self::AccountId,
 			Balance = Self::CurrencyBalance,
 		>;
+		/// Balance type of chain
 		type CurrencyBalance: Into<U256> + TryFrom<U256> + PartialEq<u128> + From<u128> + Into<u128>;
 
+		/// Decimals of balance
 		type Decimals: Get<u8>;
+		/// Collection name
 		type Name: Get<String>;
+		/// Collection symbol
 		type Symbol: Get<String>;
 
+		/// Weight information
 		type WeightInfo: WeightInfo;
 	}
 	#[pallet::pallet]
 	pub struct Pallet<T>(_);
-
-	// #[pallet::call]
-	impl<T: Config> Pallet<T> {
-		pub fn dummy() -> DispatchResult {
-			Ok(())
-		}
-	}
 }
