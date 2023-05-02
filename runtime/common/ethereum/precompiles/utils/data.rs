21d25fe4719c47cbbabf537da7c687dfe9842d0d
Trubnikov Sergey <bugrazoid@yandex.ru>
2023-05-02 12:31:28 +0000

    remove gpl license header

--- runtime/common/ethereum/precompiles/utils/data.rs
+++ runtime/common/ethereum/precompiles/utils/data.rs
@@ -1,21 +1,3 @@
-// Copyright 2019-2022 PureStake Inc.
-// Copyright 2022      Stake Technologies
-// This file is part of Utils package, originally developed by Purestake Inc.
-// Utils package used in Astar Network in terms of GPLv3.
-//
-// Utils is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-
-// Utils is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-
-// You should have received a copy of the GNU General Public License
-// along with Utils.  If not, see <http://www.gnu.org/licenses/>.
-
 use super::{EvmResult, Gasometer};
 
 use sp_std::borrow::ToOwned;
