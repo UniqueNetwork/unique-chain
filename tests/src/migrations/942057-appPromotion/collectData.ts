d47d7f05f33d05afeef9486b5e70a3e89c5a2a51
Max Andreev <maksandre@yandex.ru>
2023-06-09 19:49:23 +0000

    resolve paths

--- tests/src/migrations/942057-appPromotion/collectData.ts
+++ tests/src/migrations/942057-appPromotion/collectData.ts
@@ -1,3 +1,15 @@
-import {spawnSync} from 'child_process';
+import {execSync} from 'child_process';
+import path from 'path';
+import {dirname} from 'path';
+import {fileURLToPath} from 'url';
 
-export const collectData = () => spawnSync('sh', ['./collect-data.sh']);
+export const collectData = () => {
+  const dirName = dirname(fileURLToPath(import.meta.url));
+
+  const pathToScript = path.resolve(dirName, './stakersParser.jsonnet');
+  const outputPath = path.resolve(dirName, './output.json');
+  // execSync(`chainql --tla-str=chainUrl=wss://ws-quartz.unique.network:443 ${pathToScript} > ${outputPath}`);
+  execSync(`chainql --tla-str=chainUrl=ws://127.0.0.1:9944 ${pathToScript} > ${outputPath}`);
+};
+
+collectData();
