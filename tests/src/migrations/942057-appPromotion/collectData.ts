59488319c396d842ec604e77c3fbb3b1956b091b
Max Andreev <maksandre@yandex.ru>
2023-06-09 21:16:02 +0000

    remove testing artifacts

--- tests/src/migrations/942057-appPromotion/collectData.ts
+++ tests/src/migrations/942057-appPromotion/collectData.ts
@@ -11,5 +11,3 @@ export const collectData = () => {
   // execSync(`chainql --tla-str=chainUrl=wss://ws-quartz.unique.network:443 ${pathToScript} > ${outputPath}`);
   execSync(`chainql --tla-str=chainUrl=ws://127.0.0.1:9944 ${pathToScript} > ${outputPath}`);
 };
-
-collectData();
