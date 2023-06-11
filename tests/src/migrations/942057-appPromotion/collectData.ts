88e539da3a4c3de3a5d11d9f8dc9428945475d39
Max Andreev <maksandre@yandex.ru>
2023-06-11 08:56:09 +0000

    use exec

--- tests/src/migrations/942057-appPromotion/collectData.ts
+++ tests/src/migrations/942057-appPromotion/collectData.ts
@@ -1,4 +1,4 @@
-import {execSync} from 'child_process';
+import {exec, execSync} from 'child_process';
 import path from 'path';
 import {dirname} from 'path';
 import {fileURLToPath} from 'url';
@@ -9,5 +9,6 @@ export const collectData = () => {
   const pathToScript = path.resolve(dirName, './stakersParser.jsonnet');
   const outputPath = path.resolve(dirName, './output.json');
   // execSync(`chainql --tla-str=chainUrl=wss://ws-quartz.unique.network:443 ${pathToScript} > ${outputPath}`);
-  execSync(`chainql --tla-str=chainUrl=ws://127.0.0.1:9944 ${pathToScript} > ${outputPath}`);
+  // execSync(`chainql --tla-str=chainUrl=ws://127.0.0.1:9944 ${pathToScript} > ${outputPath}`);
+  exec(`chainql --tla-str=chainUrl=wss://ws-quartz.unique.network:443 ${pathToScript} > ${outputPath}`);
 };
