782eb4ad16aa6ba31c0ad68b0722034d87805c07
Yaroslav Bolyukin <iam@lach.pw>
2023-06-16 09:33:19 +0200

    feat: update BaseFee storage from pallet-configuration
    Signed-off-by: Yaroslav Bolyukin <iam@lach.pw>

--- pallets/configuration/src/lib.rs
+++ pallets/configuration/src/lib.rs
@@ -43,10 +43,14 @@ mod pallet {
 	use super::*;
 	use frame_support::{
 		traits::{fungible, Get},
-		pallet_prelude::{StorageValue, ValueQuery, DispatchResult, IsType},
+		pallet_prelude::*,
 		log,
 	};
-	use frame_system::{pallet_prelude::OriginFor, ensure_root, Config as SystemConfig};
+	use frame_system::{
+		pallet_prelude::{OriginFor, BlockNumberFor},
+		ensure_root, Config as SystemConfig,
+	};
+	use sp_arithmetic::Permill;
 
 	pub use crate::weights::WeightInfo;
 	pub type BalanceOf<T> =
@@ -99,6 +103,46 @@ mod pallet {
 		NewCollatorKickThreshold {
 			length_in_blocks: Option<T::BlockNumber>,
 		},
+	}
+
+	fn update_base_fee<T: Config>() {
+		let base_fee_per_gas: U256 = <MinGasPriceOverride<T>>::get().into();
+		let elasticity: Permill = Permill::zero();
+		// twox_128(BaseFee) ++ twox_128(BaseFeePerGas)
+		sp_io::storage::set(
+			&hex_literal::hex!("c1fef3b7207c11a52df13c12884e77263864ade243c642793ebcfe9e16f454ca"),
+			&base_fee_per_gas.encode(),
+		);
+		// twox_128(BaseFee) ++ twox_128(Elasticity)
+		sp_io::storage::set(
+			&hex_literal::hex!("c1fef3b7207c11a52df13c12884e772609bc3a1e532c9cb85d57feed02cbff8e"),
+			&elasticity.encode(),
+		);
+	}
+
+	/// We update our default weights on every release
+	#[pallet::hooks]
+	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {
+		fn on_runtime_upgrade() -> Weight {
+			T::DbWeight::get().reads_writes(1, 2)
+		}
+	}
+
+	#[pallet::genesis_config]
+	pub struct GenesisConfig<T>(PhantomData<T>);
+
+	#[cfg(feature = "std")]
+	impl<T: Config> Default for GenesisConfig<T> {
+		fn default() -> Self {
+			Self(Default::default())
+		}
+	}
+
+	#[pallet::genesis_build]
+	impl<T: Config> GenesisBuild<T> for GenesisConfig<T> {
+		fn build(&self) {
+			update_base_fee::<T>();
+		}
 	}
 
 	#[pallet::error]
@@ -171,6 +215,9 @@ mod pallet {
 			} else {
 				<MinGasPriceOverride<T>>::kill();
 			}
+			// This code should not be called in production, but why keep development in the
+			// inconsistent state
+			update_base_fee::<T>();
 			Ok(())
 		}
 
