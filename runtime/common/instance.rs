55ce0a62aad1d4c0237216ea97f9285e2a45ab7e
Yaroslav Bolyukin <iam@lach.pw>
2023-06-16 09:33:16 +0200

    refactor: drop TransactionConverter support
    Signed-off-by: Yaroslav Bolyukin <iam@lach.pw>

--- runtime/common/instance.rs
+++ runtime/common/instance.rs
@@ -1,6 +1,6 @@
 use crate::{
 	runtime_common::{
-		config::ethereum::CrossAccountId, ethereum::transaction_converter::TransactionConverter,
+		config::ethereum::CrossAccountId,
 	},
 	Runtime,
 };
@@ -8,9 +8,4 @@ use up_common::types::opaque::RuntimeInstance;
 
 impl RuntimeInstance for Runtime {
 	type CrossAccountId = CrossAccountId;
-	type TransactionConverter = TransactionConverter;
-
-	fn get_transaction_converter() -> TransactionConverter {
-		TransactionConverter
-	}
 }
