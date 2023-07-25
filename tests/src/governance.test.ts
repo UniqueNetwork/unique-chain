c9c017eda4fc468271a9fce100923a0314006e3f
Daniel Shiposha <mrshiposha@gmail.com>
2023-07-25 14:13:26 +0200

    test: fellowship basic tests

--- tests/src/governance.test.ts
+++ tests/src/governance.test.ts
@@ -1,5 +1,8 @@
-import {IKeyringPair} from '@polkadot/types/types/interfaces';
+import { IKeyringPair } from '@polkadot/types/types';
 import {usingPlaygrounds, itSub, expect, Pallets, requirePalletsOrSkip} from './util';
+import { Event } from './util/playgrounds/unique.dev';
+import { UniqueHelper } from './util/playgrounds/unique';
+import { DAYS, MINUTES } from './util/playgrounds/types';
 
 
 describe('Governance: Council tests', () => {
@@ -411,97 +414,230 @@ describe('Governance: Technical Committee tests', () => {
 });
 
 describe('Governance: Fellowship tests', () => {
+  const numMembersInRank = 3;
+  const memberBalance = 50_000n;
+  const rankLimit = 7;
+  const members: IKeyringPair[][] = [];
+
+  let rank1Proposer: IKeyringPair;
+
+  let sudoer: any;
+  let donor: any;
+
+  const submissionDeposit = 1000n;
+  const decisionDeposit = 10n;
+
+  const democracyLaunchPeriod = 7 * DAYS;
+  const democracyVotingPeriod = 7 * DAYS;
+  const democracyTrackId = 10;
+  const democracyTrackMinRank = 3;
+  const fellowshipPropositionOrigin = 'FellowshipProposition';
+
+  const fellowshipPreparePeriod = 30 * MINUTES;
+  const fellowshipConfirmPeriod = 30 * MINUTES;
+
+  const defaultEnactmentMoment = { After: 50 };
+
+  const maxWaitBlocks = 5;
+
+  let dummyProposalCount = 0;
+  function dummyProposal(helper: UniqueHelper) {
+      dummyProposalCount++;
+      return {
+        Inline: helper.constructApiCall("api.tx.system.remark", ["dummy proposal" + dummyProposalCount]).method.toHex()
+      };
+  }
+
+  async function voteUnanimouslyInFellowship(helper: UniqueHelper, minRank: number, referendumIndex: number) {
+      for (var rank = minRank; rank < rankLimit; rank++) {
+        for (var member of members[rank]) {
+          await helper.fellowship.collective.vote(member, referendumIndex, true);
+        }
+      }
+  }
+
   before(async function() {
-    await usingPlaygrounds(async (helper) => {
-      requirePalletsOrSkip(this, helper, [Pallets.Fellowship]);
-    });
+      await usingPlaygrounds(async (helper, privateKey) => {
+        requirePalletsOrSkip(this, helper, [Pallets.Democracy, Pallets.Fellowship]);
+
+        sudoer = await privateKey('//Alice');
+
+        donor = await privateKey({url: import.meta.url});
+
+        for (var i = 0; i < rankLimit; i++) {
+          const rankMembers = await helper.arrange.createAccounts(
+              Array(numMembersInRank).fill(memberBalance),
+              donor,
+          )
+
+          for (var member of rankMembers) {
+              await helper.getSudo().fellowship.collective.addMember(sudoer, member.address);
+
+              for (var rank = 0; rank < i; rank++) {
+                await helper.getSudo().fellowship.collective.promote(sudoer, member.address);
+              }
+          }
+
+          members.push(rankMembers);
+        }
+
+        rank1Proposer = members[1][0];
+      });
   });
 
-  itSub.skip('FellowshipDemocracyProposal can submit regular Democracy proposals', async (helper) => {
+  itSub('FellowshipProposal can submit regular Democracy proposals', async ({helper}) => {
+      const democracyProposal = dummyProposal(helper);
+      const fellowshipProposal = {
+        Inline: helper.constructApiCall("api.tx.democracy.propose", [democracyProposal, 0]).method.toHex(),
+      };
+
+      const submitResult = await helper.fellowship.referenda.submit(
+        rank1Proposer,
+        fellowshipPropositionOrigin,
+        fellowshipProposal,
+        { After: 0 },
+      );
+
+      const fellowshipReferendumIndex = Event.FellowshipReferenda.Submitted.expect(submitResult).referendumIndex;
+      await voteUnanimouslyInFellowship(helper, democracyTrackMinRank, fellowshipReferendumIndex);
+      await helper.fellowship.referenda.placeDecisionDeposit(donor, fellowshipReferendumIndex);
+
+      const democracyProposed = await helper.wait.expectEvent(fellowshipPreparePeriod + fellowshipConfirmPeriod, Event.Democracy.Proposed);
+
+      const publicProposals: any[] = await helper.democracy.publicProposals();
+      const democracyEnqueuedProposal = publicProposals.find(proposalInfo => {
+        proposalInfo[0] == democracyProposed.proposalIndex
+        && proposalInfo[1].inline == democracyProposal.Inline
+      });
+      expect(democracyEnqueuedProposal, "Fellowship proposal expected to be in the Democracy").to.be.not.null;
+
+      await helper.wait.newBlocks(donor, democracyVotingPeriod);
+  });
+
+  itSub('Fellowship (rank-1 or greater) member can submit Fellowship proposals on the Democracy track', async ({helper}) => {
+      for (var rank = 1; rank < rankLimit; rank++) {
+        const rankMembers = members[rank];
+        
+        for (var memberIdx = 0; memberIdx < rankMembers.length; memberIdx++) {
+          const member = rankMembers[memberIdx];
+          const newDummyProposal = dummyProposal(helper);
+
+          const submitResult = await helper.fellowship.referenda.submit(
+              member,
+              fellowshipPropositionOrigin,
+              newDummyProposal,
+              defaultEnactmentMoment,
+          );
+
+          const referendumIndex = Event.FellowshipReferenda.Submitted.expect(submitResult).referendumIndex;
+          const referendumInfo = await helper.fellowship.referenda.referendumInfo(referendumIndex);
+          expect(referendumInfo.ongoing.track, `${memberIdx}-th member of rank #${rank}: proposal #${referendumIndex} is on invalid track`)
+              .to.be.equal(democracyTrackId);
+        }
+      }
+  });
+
+  itSub('Fellowship (rank-3 or greater) members can vote on the Democracy track', async ({helper}) => {
+      const proposal = dummyProposal(helper);
+
+      const submitResult = await helper.fellowship.referenda.submit(
+        rank1Proposer,
+        fellowshipPropositionOrigin,
+        proposal,
+        defaultEnactmentMoment
+      );
+
+      const referendumIndex = Event.FellowshipReferenda.Submitted.expect(submitResult).referendumIndex;
+
+      let expectedAyes = 0;
+      for (var rank = 3; rank < rankLimit; rank++) {
+        const rankMembers = members[rank];
+
+        for (var memberIdx = 0; memberIdx < rankMembers.length; memberIdx++) {
+          const member = rankMembers[memberIdx];
+          await helper.fellowship.collective.vote(member, referendumIndex, true);
+          expectedAyes += 1;
+
+          const referendumInfo = await helper.fellowship.referenda.referendumInfo(referendumIndex);
+          expect(referendumInfo.ongoing.tally.bareAyes, `Vote from ${memberIdx}-th member of rank #${rank} isn't accounted`)
+              .to.be.equal(expectedAyes);
+        }
+      }
+  });
+
+  itSub.skip('Fellowship rank vote strength is correct', async ({helper}) => {
 
   });
 
-  itSub.skip('Fellowship (rank-1  or greater) member can submit Fellowship proposals on the Democracy track', async (helper) => {
+  itSub.skip('[Negative] FellowshipProposal cannot externally propose SuperMajorityAgainst', async ({helper}) => {
 
   });
 
-  itSub.skip('Fellowship (rank-3 or greater) members can vote on the Democracy track', async (helper) => {
+  itSub.skip('[Negative] FellowshipProposal cannot externally propose SimpleMajority', async ({helper}) => {
 
   });
 
-  itSub.skip('Fellowship rank vote strength is correct', async (helper) => {
+  itSub.skip('[Negative] FellowshipProposal cannot externally propose SuperMajorityApprove', async ({helper}) => {
 
   });
 
-  itSub.skip('[Negative] FellowshipDemocracyProposal cannot externally propose SuperMajorityAgainst', async (helper) => {
+  itSub.skip('[Negative] Fellowship (rank-0) member cannot submit Fellowship proposals on the Democracy track', async ({helper}) => {
 
   });
 
-  itSub.skip('[Negative] FellowshipDemocracyProposal cannot externally propose SimpleMajority', async (helper) => {
+  itSub.skip('[Negative] Fellowship (rank-1 or greater) member cannot submit if no deposit can be provided', async ({helper}) => {
 
   });
 
-  itSub.skip('[Negative] FellowshipDemocracyProposal cannot externally propose SuperMajorityApprove', async (helper) => {
+  itSub.skip('[Negative] Fellowship (rank-2 or less) members cannot vote on the Democracy track', async ({helper}) => {
 
   });
 
-  itSub.skip('[Negative] Fellowship (rank-0) member cannot submit Fellowship proposals on the Democracy track', async (helper) => {
+  itSub.skip('[Negative] FellowshipProposal cannot add/remove a Council member', async ({helper}) => {
 
   });
 
-  itSub.skip('[Negative] Fellowship (rank-1 or greater) member cannot submit if no deposit can be provided', async (helper) => {
+  itSub.skip('[Negative] FellowshipProposal cannot set Council prime member', async ({helper}) => {
 
   });
 
-  itSub.skip('[Negative] Fellowship (rank-2 or less) members cannot vote on the Democracy track', async (helper) => {
+  itSub.skip('[Negative] FellowshipProposal member cannot set Council prime member', async ({helper}) => {
 
   });
 
-  itSub.skip('[Negative] FellowshipDemocracyProposal cannot add/remove a Council member', async (helper) => {
+  itSub.skip('[Negative] FellowshipProposal cannot add/remove a TechComm member', async ({helper}) => {
 
   });
 
-  itSub.skip('[Negative] FellowshipDemocracyProposal cannot set Council prime member', async (helper) => {
+  itSub.skip('[Negative] FellowshipProposal cannot add/remove a Fellowship member', async ({helper}) => {
 
   });
 
-  itSub.skip('[Negative] FellowshipDemocracyProposal member cannot set Council prime member', async (helper) => {
+  itSub.skip('[Negative] FellowshipProposal cannot promote/demote a Fellowship member', async ({helper}) => {
 
   });
 
-  itSub.skip('[Negative] FellowshipDemocracyProposal cannot add/remove a TechComm member', async (helper) => {
+  itSub.skip('[Negative] FellowshipProposal cannot fast-track Democracy proposals', async ({helper}) => {
 
   });
 
-  itSub.skip('[Negative] FellowshipDemocracyProposal cannot add/remove a Fellowship member', async (helper) => {
+  itSub.skip('[Negative] FellowshipProposal cannot cancel approved Democracy proposals', async ({helper}) => {
 
   });
 
-  itSub.skip('[Negative] FellowshipDemocracyProposal cannot promote/demote a Fellowship member', async (helper) => {
+  itSub.skip('[Negative] FellowshipProposal cannot cancel ongoing Democracy referendums', async ({helper}) => {
 
   });
 
-  itSub.skip('[Negative] FellowshipDemocracyProposal cannot fast-track Democracy proposals', async (helper) => {
+  itSub.skip('[Negative] FellowshipProposal cannot cancel queued Democracy proposals', async ({helper}) => {
 
   });
 
-  itSub.skip('[Negative] FellowshipDemocracyProposal cannot cancel approved Democracy proposals', async (helper) => {
+  itSub.skip('[Negative] FellowshipProposal cannot blacklist Democracy proposals', async ({helper}) => {
 
   });
 
-  itSub.skip('[Negative] FellowshipDemocracyProposal cannot cancel ongoing Democracy referendums', async (helper) => {
-
-  });
-
-  itSub.skip('[Negative] FellowshipDemocracyProposal cannot cancel queued Democracy proposals', async (helper) => {
-
-  });
-
-  itSub.skip('[Negative] FellowshipDemocracyProposal cannot blacklist Democracy proposals', async (helper) => {
-
-  });
-
-  itSub.skip('[Negative] FellowshipDemocracyProposal cannot veto Democracy proposals', async (helper) => {
+  itSub.skip('[Negative] FellowshipProposal cannot veto Democracy proposals', async ({helper}) => {
 
   });
 });
@@ -521,15 +657,15 @@ describe('Governance: Democracy tests', () => {
 
   });
 
-  itSub.skip('[Negative] Regular user cannot externally propose SuperMajorityAgainst', async (helper) => {
+  itSub.skip('[Negative] Regular user cannot externally propose SuperMajorityAgainst', async ({helper}) => {
 
   });
 
-  itSub.skip('[Negative] Regular user cannot externally propose SimpleMajority', async (helper) => {
+  itSub.skip('[Negative] Regular user cannot externally propose SimpleMajority', async ({helper}) => {
 
   });
 
-  itSub.skip('[Negative] Regular user cannot externally propose SuperMajorityApprove', async (helper) => {
+  itSub.skip('[Negative] Regular user cannot externally propose SuperMajorityApprove', async ({helper}) => {
 
   });
 });
