9025c9474593e400e24c06e5a0c139f713e9ed65
PraetorP <praetorian281@gmail.com>
2023-08-18 14:28:51 +0000

    fix(runtime): spces -> tabs

--- runtime/common/construct_runtime.rs
+++ runtime/common/construct_runtime.rs
@@ -16,129 +16,129 @@
 
 #[macro_export]
 macro_rules! construct_runtime {
-    () => {
-        frame_support::construct_runtime! {
+	() => {
+		frame_support::construct_runtime! {
 
-            pub enum Runtime where
-                Block = Block,
-                NodeBlock = opaque::Block,
-                UncheckedExtrinsic = UncheckedExtrinsic
-            {
-                System: frame_system = 0,
-                StateTrieMigration: pallet_state_trie_migration = 1,
+			pub enum Runtime where
+				Block = Block,
+				NodeBlock = opaque::Block,
+				UncheckedExtrinsic = UncheckedExtrinsic
+			{
+				System: frame_system = 0,
+				StateTrieMigration: pallet_state_trie_migration = 1,
 
-                ParachainSystem: cumulus_pallet_parachain_system = 20,
-                ParachainInfo: parachain_info = 21,
+				ParachainSystem: cumulus_pallet_parachain_system = 20,
+				ParachainInfo: parachain_info = 21,
 
-                #[cfg(feature = "collator-selection")]
-                Authorship: pallet_authorship = 22,
+				#[cfg(feature = "collator-selection")]
+				Authorship: pallet_authorship = 22,
 
-                #[cfg(feature = "collator-selection")]
-                CollatorSelection: pallet_collator_selection = 23,
+				#[cfg(feature = "collator-selection")]
+				CollatorSelection: pallet_collator_selection = 23,
 
-                #[cfg(feature = "collator-selection")]
-                Session: pallet_session = 24,
+				#[cfg(feature = "collator-selection")]
+				Session: pallet_session = 24,
 
-                Aura: pallet_aura = 25,
-                AuraExt: cumulus_pallet_aura_ext = 26,
+				Aura: pallet_aura = 25,
+				AuraExt: cumulus_pallet_aura_ext = 26,
 
-                Balances: pallet_balances = 30,
-                // RandomnessCollectiveFlip = 31
-                Timestamp: pallet_timestamp = 32,
-                TransactionPayment: pallet_transaction_payment = 33,
-                Treasury: pallet_treasury = 34,
-                Sudo: pallet_sudo = 35,
-                Vesting: orml_vesting = 37,
+				Balances: pallet_balances = 30,
+				// RandomnessCollectiveFlip = 31
+				Timestamp: pallet_timestamp = 32,
+				TransactionPayment: pallet_transaction_payment = 33,
+				Treasury: pallet_treasury = 34,
+				Sudo: pallet_sudo = 35,
+				Vesting: orml_vesting = 37,
 
-                XTokens: orml_xtokens = 38,
-                Tokens: orml_tokens = 39,
-                // Contracts: pallet_contracts::{Pallet, Call, Storage, Event<T>} = 38,
+				XTokens: orml_xtokens = 38,
+				Tokens: orml_tokens = 39,
+				// Contracts: pallet_contracts::{Pallet, Call, Storage, Event<T>} = 38,
 
-                #[cfg(feature = "collator-selection")]
-                Identity: pallet_identity = 40,
+				#[cfg(feature = "collator-selection")]
+				Identity: pallet_identity = 40,
 
-                #[cfg(feature = "preimage")]
-                Preimage: pallet_preimage::{Pallet, Call, Storage, Event<T>} = 41,
+				#[cfg(feature = "preimage")]
+				Preimage: pallet_preimage::{Pallet, Call, Storage, Event<T>} = 41,
 
-                #[cfg(feature = "governance")]
-                Democracy: pallet_democracy::{Pallet, Call, Storage, Config<T>, Event<T>} = 42,
+				#[cfg(feature = "governance")]
+				Democracy: pallet_democracy::{Pallet, Call, Storage, Config<T>, Event<T>} = 42,
 
-                #[cfg(feature = "governance")]
-                Council: pallet_collective::<Instance1>::{Pallet, Call, Storage, Origin<T>, Event<T>, Config<T>} = 43,
+				#[cfg(feature = "governance")]
+				Council: pallet_collective::<Instance1>::{Pallet, Call, Storage, Origin<T>, Event<T>, Config<T>} = 43,
 
-                #[cfg(feature = "governance")]
-                TechnicalCommittee: pallet_collective::<Instance2>::{Pallet, Call, Storage, Origin<T>, Event<T>, Config<T>} = 44,
+				#[cfg(feature = "governance")]
+				TechnicalCommittee: pallet_collective::<Instance2>::{Pallet, Call, Storage, Origin<T>, Event<T>, Config<T>} = 44,
 
-                #[cfg(feature = "governance")]
-                CouncilMembership: pallet_membership::<Instance1>::{Pallet, Call, Storage, Event<T>, Config<T>} = 45,
+				#[cfg(feature = "governance")]
+				CouncilMembership: pallet_membership::<Instance1>::{Pallet, Call, Storage, Event<T>, Config<T>} = 45,
 
-                #[cfg(feature = "governance")]
-                TechnicalCommitteeMembership: pallet_membership::<Instance2>::{Pallet, Call, Storage, Event<T>, Config<T>} = 46,
+				#[cfg(feature = "governance")]
+				TechnicalCommitteeMembership: pallet_membership::<Instance2>::{Pallet, Call, Storage, Event<T>, Config<T>} = 46,
 
-                #[cfg(feature = "governance")]
-                FellowshipCollective: pallet_ranked_collective::{Pallet, Call, Storage, Event<T>} = 47,
+				#[cfg(feature = "governance")]
+				FellowshipCollective: pallet_ranked_collective::{Pallet, Call, Storage, Event<T>} = 47,
 
-                #[cfg(feature = "governance")]
-                FellowshipReferenda: pallet_referenda::{Pallet, Call, Storage, Event<T>} = 48,
+				#[cfg(feature = "governance")]
+				FellowshipReferenda: pallet_referenda::{Pallet, Call, Storage, Event<T>} = 48,
 
-                #[cfg(feature = "governance")]
-                GovScheduler: pallet_scheduler::{Pallet, Call, Storage, Event<T>} = 49,
+				#[cfg(feature = "governance")]
+				GovScheduler: pallet_scheduler::{Pallet, Call, Storage, Event<T>} = 49,
 
-                #[cfg(feature = "governance")]
-                Origins: pallet_gov_origins::{Origin} = 99,
+				#[cfg(feature = "governance")]
+				Origins: pallet_gov_origins::{Origin} = 99,
 
-                // XCM helpers.
-                XcmpQueue: cumulus_pallet_xcmp_queue = 50,
-                PolkadotXcm: pallet_xcm = 51,
-                CumulusXcm: cumulus_pallet_xcm = 52,
-                DmpQueue: cumulus_pallet_dmp_queue = 53,
+				// XCM helpers.
+				XcmpQueue: cumulus_pallet_xcmp_queue = 50,
+				PolkadotXcm: pallet_xcm = 51,
+				CumulusXcm: cumulus_pallet_xcm = 52,
+				DmpQueue: cumulus_pallet_dmp_queue = 53,
 
-                // Unique Pallets
-                Inflation: pallet_inflation = 60,
-                Unique: pallet_unique::{Pallet, Call, Storage} = 61,
+				// Unique Pallets
+				Inflation: pallet_inflation = 60,
+				Unique: pallet_unique::{Pallet, Call, Storage} = 61,
 
-                // #[cfg(feature = "scheduler")]
-                // Scheduler: pallet_unique_scheduler_v2 = 62,
+				// #[cfg(feature = "scheduler")]
+				// Scheduler: pallet_unique_scheduler_v2 = 62,
 
-                Configuration: pallet_configuration = 63,
+				Configuration: pallet_configuration = 63,
 
-                Charging: pallet_charge_transaction = 64,
-                // ContractHelpers: pallet_contract_helpers::{Pallet, Call, Storage} = 65,
-                Common: pallet_common = 66,
-                Fungible: pallet_fungible = 67,
+				Charging: pallet_charge_transaction = 64,
+				// ContractHelpers: pallet_contract_helpers::{Pallet, Call, Storage} = 65,
+				Common: pallet_common = 66,
+				Fungible: pallet_fungible = 67,
 
-                #[cfg(feature = "refungible")]
-                Refungible: pallet_refungible = 68,
+				#[cfg(feature = "refungible")]
+				Refungible: pallet_refungible = 68,
 
-                Nonfungible: pallet_nonfungible = 69,
-                Structure: pallet_structure = 70,
+				Nonfungible: pallet_nonfungible = 69,
+				Structure: pallet_structure = 70,
 
-                // RmrkCore: pallet_proxy_rmrk_core = 71,
+				// RmrkCore: pallet_proxy_rmrk_core = 71,
 
-                // RmrkEquip: pallet_proxy_rmrk_equip = 72,
+				// RmrkEquip: pallet_proxy_rmrk_equip = 72,
 
-                #[cfg(feature = "app-promotion")]
-                AppPromotion: pallet_app_promotion = 73,
+				#[cfg(feature = "app-promotion")]
+				AppPromotion: pallet_app_promotion = 73,
 
-                #[cfg(feature = "foreign-assets")]
-                ForeignAssets: pallet_foreign_assets = 80,
+				#[cfg(feature = "foreign-assets")]
+				ForeignAssets: pallet_foreign_assets = 80,
 
-                // Frontier
-                EVM: pallet_evm = 100,
-                Ethereum: pallet_ethereum = 101,
+				// Frontier
+				EVM: pallet_evm = 100,
+				Ethereum: pallet_ethereum = 101,
 
-                EvmCoderSubstrate: pallet_evm_coder_substrate = 150,
-                EvmContractHelpers: pallet_evm_contract_helpers = 151,
-                EvmTransactionPayment: pallet_evm_transaction_payment = 152,
-                EvmMigration: pallet_evm_migration = 153,
+				EvmCoderSubstrate: pallet_evm_coder_substrate = 150,
+				EvmContractHelpers: pallet_evm_contract_helpers = 151,
+				EvmTransactionPayment: pallet_evm_transaction_payment = 152,
+				EvmMigration: pallet_evm_migration = 153,
 
-                Maintenance: pallet_maintenance = 154,
+				Maintenance: pallet_maintenance = 154,
 
-                BalancesAdapter: pallet_balances_adapter = 155,
+				BalancesAdapter: pallet_balances_adapter = 155,
 
-                #[cfg(feature = "test-env")]
-                TestUtils: pallet_test_utils = 255,
-            }
-        }
-    };
+				#[cfg(feature = "test-env")]
+				TestUtils: pallet_test_utils = 255,
+			}
+		}
+	};
 }
