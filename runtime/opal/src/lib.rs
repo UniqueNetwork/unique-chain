bdb29fd7c4877d7af36d28b07ce084282166b982
Yaroslav Bolyukin <iam@lach.pw>
2023-10-24 14:17:35 +0200

    chore: bump spec_version to 10010063

--- runtime/opal/src/lib.rs
+++ runtime/opal/src/lib.rs
@@ -49,7 +49,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("opal"),
 	impl_name: create_runtime_str!("opal"),
 	authoring_version: 1,
-	spec_version: 10010062,
+	spec_version: 10010063,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 3,
--- runtime/quartz/src/lib.rs
+++ runtime/quartz/src/lib.rs
@@ -51,7 +51,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	impl_name: create_runtime_str!("quartz"),
 
 	authoring_version: 1,
-	spec_version: 10010062,
+	spec_version: 10010063,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 3,
@@ -64,7 +64,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	impl_name: create_runtime_str!("sapphire"),
 
 	authoring_version: 1,
-	spec_version: 10010062,
+	spec_version: 10010063,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 3,
