0744c08ebf2266d31ce80f3c3717a9b2c2ad3cd9
Daniel Shiposha <mrshiposha@gmail.com>
2023-12-19 18:36:32 +0100

    feat: improve fast track script

--- js-packages/scripts/proposefasttrack.ts
+++ js-packages/scripts/proposefasttrack.ts
@@ -3,19 +3,24 @@ import {blake2AsHex} from '@polkadot/util-crypto';
 
 async function main() {
   const networkUrl = process.argv[2];
+  const proposal = process.argv[3];
 
   const wsProvider = new WsProvider(networkUrl);
   const api = await ApiPromise.create({provider: wsProvider});
 
-  const externalDemocracyProposal = (await api.query.democracy.nextExternal() as any).unwrap()[0];
-
   let proposalHash;
-  if(externalDemocracyProposal.isInline) {
-    proposalHash = blake2AsHex(externalDemocracyProposal.asInline, 256);
-  } else if(externalDemocracyProposal.isLegacy) {
-    proposalHash = externalDemocracyProposal.asLegacy.toJSON().hash;
+
+  if(proposal == 'existing-proposal') {
+    const externalDemocracyProposal = (await api.query.democracy.nextExternal() as any).unwrap()[0];
+    if(externalDemocracyProposal.isInline) {
+      proposalHash = blake2AsHex(externalDemocracyProposal.asInline, 256);
+    } else if(externalDemocracyProposal.isLegacy) {
+      proposalHash = externalDemocracyProposal.asLegacy.toJSON().hash;
+    } else {
+      proposalHash = externalDemocracyProposal.asLookup.toJSON().hash;
+    }
   } else {
-    proposalHash = externalDemocracyProposal.asLookup.toJSON().hash;
+    proposalHash = proposal;
   }
 
   const voringPeriod = 7200;
