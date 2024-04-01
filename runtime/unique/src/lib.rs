dea010c27497f6a02416a2aae09b582c34d1f9bf
Daniel Shiposha <mrshiposha@gmail.com>
2024-04-01 12:14:31 +0200

    chore: bump spec_version

--- runtime/unique/src/lib.rs
+++ runtime/unique/src/lib.rs
@@ -49,7 +49,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("unique"),
 	impl_name: create_runtime_str!("unique"),
 	authoring_version: 1,
-	spec_version: 10030070,
+	spec_version: 10030071,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 3,
