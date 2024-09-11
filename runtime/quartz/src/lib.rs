0b6e8e822888da5197f7fe8a3f3840b0e9ece7ed
Daniel Shiposha <mrshiposha@gmail.com>
2024-09-11 10:53:27 +0200

    fix quartz/unique register_validate_block

--- runtime/quartz/src/lib.rs
+++ runtime/quartz/src/lib.rs
@@ -94,5 +94,4 @@ impl_common_runtime_apis!();
 cumulus_pallet_parachain_system::register_validate_block!(
 	Runtime = Runtime,
 	BlockExecutor = cumulus_pallet_aura_ext::BlockExecutor::<Runtime, Executive>,
-	CheckInherents = CheckInherents,
 );
