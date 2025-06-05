fb84f4635d1c23617db3b02e4557d552b345ff73
Grigoriy Simonov <gsimonov@usetech.com>
2025-06-05 15:48:23 +0000

    update spec version

--- runtime/opal/src/lib.rs
+++ runtime/opal/src/lib.rs
@@ -51,7 +51,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("opal"),
 	impl_name: create_runtime_str!("opal"),
 	authoring_version: 1,
-	spec_version: 10170081,
+	spec_version: 10180082,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 3,
--- runtime/unique/src/lib.rs
+++ runtime/unique/src/lib.rs
@@ -51,7 +51,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("unique"),
 	impl_name: create_runtime_str!("unique"),
 	authoring_version: 1,
-	spec_version: 10170081,
+	spec_version: 10180082,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 3,
