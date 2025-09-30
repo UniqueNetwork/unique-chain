aedbea5a2faeb4272c66ebbc3e75c668060402e4
Yaroslav Bolyukin <iam@lach.pw>
2025-09-30 05:09:17 +0200

    test: post deno typing fix

--- js-packages/scripts/proposefasttrack.ts
+++ js-packages/scripts/proposefasttrack.ts
@@ -1,6 +1,6 @@
 import {ApiPromise, WsProvider} from '@polkadot/api';
 import {blake2AsHex} from '@polkadot/util-crypto';
-import {DAYS} from './blocktimes';
+import {DAYS} from './blocktimes.ts';
 import process from "node:process";
 
 async function main() {
