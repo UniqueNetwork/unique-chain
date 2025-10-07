df7cfc68116214537093f632604261cca637bb53
Grigoriy Simonov <gsimonov@usetech.com>
2025-10-07 23:17:21 +0000

    ci: cargo fmt

--- runtime/common/config/parachain.rs
+++ runtime/common/config/parachain.rs
@@ -18,11 +18,7 @@ use cumulus_pallet_parachain_system::{
 	consensus_hook::UnincludedSegmentCapacity, RelayChainStateProof,
 };
 use cumulus_primitives_core::AggregateMessageOrigin;
-use frame_support::{
-	parameter_types,
-	traits::EnqueueWithOrigin,
-	weights::Weight,
-};
+use frame_support::{parameter_types, traits::EnqueueWithOrigin, weights::Weight};
 use up_common::constants::*;
 
 use crate::{MessageQueue, Runtime, RuntimeEvent, XcmpQueue};
